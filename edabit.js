2c3ec708744a81d9afed9b13b179498d4bef09dd
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-03 11:19:03 -0500

    animals

--- edabit.js
+++ edabit.js
@@ -105,3 +105,42 @@ function lessThanOrEqualToZero(num) {
 }
 
 // console.log(lessThanOrEqualToZero(0));
+
+// Recursion: Length of a String
+// Write a function that returns the length of a string. Make your function recursive.
+function length(str) {
+  // base case
+  if (str === '') {
+    return 0;
+  } else {
+    return length(str.substr(1)) + 1;
+  }
+}
+
+// console.log(length('apple'));
+
+// The Farm Problem
+// In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+//
+// chickens = 2 legs
+// cows = 4 legs
+// pigs = 4 legs
+//
+// The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+
+// Notes
+// Don't forget to return the result.
+// The order of animals passed is animals(chickens, cows, pigs).
+// Remember that the farmer wants to know the total number of legs and not the total number of animals.
+
+// function animals(chickens, cows, pigs) {
+//   return (chickens * 2) + (cows * 4) + (pigs * 4)
+// }
+
+function animals(chickens, cows, pigs) {
+  let legs = (chickens * 2) + (cows * 4) + (pigs * 4)
+  return legs;
+}
+
+console.log(animals(2, 3, 5)) // ➞ 36);
+console.log(animals(1, 2, 3)) // ➞ 22);
